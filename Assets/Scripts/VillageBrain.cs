2fa133b3d9a13bc973065f19d459160c78efe7e7
FearJudge <apa649@gmail.com>
2022-10-31 14:00:50 +0200

    Added Base Project
    Just 3rd Person Base

---
+++ Assets/Scripts/VillageBrain.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VillageBrain : MonoBehaviour
+{
+    public NPCLogic[] allVillagers = new NPCLogic[0];
+    public float loopTime = 0; // G
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
